bd27e80a772cca74ed2d5f7c5350faf2c9b78efe
Jai Deshmukh <67594689+djai-J@users.noreply.github.com>
2022-10-12 17:17:25 -0400

    working version!

--- Bluetooth/bluetooth_p1.ino
+++ Bluetooth/bluetooth_p1.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial HM10(3,2); //changed to pins 3 and 2
+SoftwareSerial HM10(8,11); //changed to pins 3 and 11
 char appData;  
 String inData = "";
 //change to arbitrary pins
@@ -11,7 +11,8 @@ void setup(){
   Serial.println("HM10 serial started at 9600");
   HM10.begin(9600); 
 
-  
+ 
+
   //pinMode(13, OUTPUT); // setting output
   //digitalWrite(13, LOW); // writing to output
 }
@@ -19,30 +20,32 @@ void setup(){
 void loop(){
   HM10.listen();  // listen the HM10 port
   if (HM10.available() > 0){
-    Serial.print("reading...")
+    Serial.println("reading...");
+    HM10.write("reading...");
   }
+  
 
   while (HM10.available() > 0){
+    //Serial.println("TEST");
     appData = HM10.read();
-    
     inData = String(appData);  // save the data in string format
-    Serial.write(appData);
-    Serial.print(inData);
-
-    if( inData == "S") 
-    {
-    Serial.println("Tractor stopped");
-    //insert tractor instructions for switching off
-    delay(500);
-    }
-    if ( inData == "G") 
-    {
-    Serial.println("Tractor Moving..");
-    //insert tractor instructions for On
-    delay(500);
-    }
-
+    //Serial.write(appData);
+    //Serial.println(inData);
   }
+  if( inData == "S") 
+  {
+  Serial.println("Tractor stopped");
+  HM10.write("Tractor stopped");
+  //insert tractor instructions for switching off
+  }
+  if ( inData == "G") 
+  {
+  Serial.println("Tractor Moving..");
+  HM10.write("Tractor moving");
+  //insert tractor instructions for On
+  }
+
+  inData = "";
 
   
 
