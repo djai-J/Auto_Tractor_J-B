897ddff37eb0472583bcd964a89d3a9873381df0
Brendan Love <112262704+loveb3@users.noreply.github.com>
2022-10-19 16:10:37 -0400

    Added the tractor autonomous main file

--- AutoTractorMain/AutoTractorMain.ino
+++ AutoTractorMain/AutoTractorMain.ino
@@ -1,10 +1,15 @@
 #include <MPU6050_light.h>
 #include <Wire.h>
+#include <SoftwareSerial.h>
+
 MPU6050 mpu(Wire);
 
-unsigned long timer = 0;
+int timer = 0;
 
+// the pin for the IR sensor
+int IR = A0;
 
+// the pins for the L298N motor driver
 int ENA = 6;
 int ENB = 5;
 int IN1 = 2;
@@ -12,7 +17,7 @@ int IN2 = 7;
 int IN3 = 12;
 int IN4 = 13;
 
-
+// the pin for the on/off button
 int offButton = A1;
 int button;
 int prevButton;
@@ -31,6 +36,7 @@ int left;
 
 float angle;
 float prevAngle;
+float desiredAngle = 0;
 int dt;
 int currTime;
 int prevTime;
@@ -39,6 +45,23 @@ int rightSpeed = 240;
 int leftSpeed = 240;
 
 int calibCount = 0;
+
+// counts the sequential turns
+int turnCount = 0;
+int masterCount = 0;
+
+int currLight = 0;
+int prevLight = 0;
+
+int wait = 0;
+
+int drive = 0;
+
+// initializations for bluetooth communication
+SoftwareSerial HM10(8,11); //changed to pins 8 and 11
+char appData;  
+String inData = "";
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -56,6 +79,9 @@ void setup() {
 
   // initialize stop button to stop the loop
   //pinMode(offButton, INPUT);
+
+  // initialize the IR pin as an input
+  pinMode(IR, INPUT);
 
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
@@ -77,11 +103,16 @@ void setup() {
   delay(1000);
   mpu.calcOffsets(true,true); // gyro and accelero
   Serial.println("Done!\n");
+
+  // Bluetooth communication rate
+  HM10.begin(9600); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
+  drive = bluetooth();
+
   currTime = millis();
   dt = currTime - prevTime;
 
@@ -95,72 +126,111 @@ void loop() {
   } else if (start == 1){
 
     obstacle = ultrasonic();
+    currLight = analogRead(IR);
 
     if(obstacle == 1){
       brake();
-    }
+    }else{
 
-    angle = mpu.getAngleZ();
-    driveStraight(angle, prevAngle, dt);
-    prevAngle = angle;
+      
+      angle = mpu.getAngleZ();
+      
+      if(timer<5){
+        timer++;
+      }else{
+        // may turn early do to unsigned int, typecast later if needed
+        if(currLight < (prevLight - 200) && turnCount < 2 && wait > 10){
+          desiredAngle = desiredAngle + 90;
+          turnCount += 1;
+          masterCount += 1;
+          wait = 0;
+        } else if(currLight < (prevLight - 200) && turnCount < 4 && wait > 10){
+          desiredAngle = desiredAngle - 90;
+          turnCount += 1;
+          masterCount += 1;
+          if(turnCount == 4){
+            turnCount = 0;
+          }
+          wait = 0;
+        } else{
+          wait += 1;
+        }
+        timer = 0;
+        Serial.println(angle);
+        drivePID(angle, prevAngle, desiredAngle, dt);
+        prevAngle = angle;
+
+        prevLight = currLight;
+      }
+      
+    }
 
   }
   
   prevTime = currTime;
 }
 
-void driveStraight(float currYaw, float prevYaw, int dt) {
 
-  float errorIntegral;
-  // Directions of motors compared to inputs IN1 and IN2
+void drivePID(float currYaw, float prevYaw, float desiredYaw, int dt) {
+
+  // Directions of left motor compared to inputs IN1 and IN2
   // IN1'IN2' = off
   // IN1'IN2 = Reverse
   // IN1IN2' = Forward
   // IN1IN2 = off
 
-  // Directions of motors compared to inputs IN3 IN4
+  // Directions of right motor compared to inputs IN3 IN4
   // IN3'IN4' = off
   // IN3'IN4 = Forward
   // IN3IN4' = Reverse
   // IN3IN4 = off
 
-  // Drive both wheels forward
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
+  // positive yaw is counterclockwise
+
+  if(abs(desiredYaw - currYaw) < 30){
+    // Drive both wheels forward
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  }else if((desiredYaw - currYaw) > 30){
+    // Turn immediately to the left
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  }else if((desiredYaw - currYaw) < -30){
+    // Turn immediately to the right
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+  }
+
+  if(currYaw == 0){
+    rightSpeed = 240;
+    leftSpeed = 240;
+  }
 
   // P term 
-  int kp = 10;
+  int kp = 1;
 
   // calculate the desired speed of the right motor
-  rightSpeed = rightSpeed + (currYaw - prevYaw) * kp;
+  rightSpeed = rightSpeed - kp * (desiredYaw - currYaw);
   if(rightSpeed > 255){
     rightSpeed = 255;
-  }else if (rightSpeed < 0){
-    rightSpeed = 0;
+  }else if (rightSpeed < 225){
+    rightSpeed = 225;
   }
 
   // calculate the desired speed of the left motor
-  leftSpeed = leftSpeed - (currYaw - prevYaw) * kp;
+  leftSpeed = leftSpeed + kp * (desiredYaw - currYaw);
   if(leftSpeed > 255){
     leftSpeed = 255;
-  }else if (leftSpeed < 0){
-    leftSpeed = 0;
+  }else if (leftSpeed < 225){
+    leftSpeed = 225;
   }
-/*
-  // I term
-  int ki = 10;
-
-
-  errorIntegral = (currYaw - prevYaw) * dt;
-
-  // right motor
-  rightSpeed += errorIntegral * ki;
-
-  // left motor
-  leftSpeed += -errorIntegral * ki;
-*/
+  
   // set motor speed
   analogWrite(ENA, rightSpeed);
   analogWrite(ENB, leftSpeed);
@@ -175,13 +245,13 @@ int run() {
   static int start = 0;
 
   button = analogRead(offButton);
-  //Serial.println(button);
+  
   if(button <= 500 && prevButton > 500 && start == 0){
     start = 1;
-    Serial.println("on xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+    //Serial.println("on xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
   } else if(button <= 500 && prevButton > 500 && start != 0){
     start = 0;
-    Serial.println("off");
+    //Serial.println("off");
   }
 
   prevButton = button;
@@ -207,18 +277,18 @@ int ultrasonic(){
   distance = (rawPulse * 0.0343)/2;
 
   
-
+  
   static int i = 0;
 
   i++;
 
-  if(i>1000){
+  if(i>500){
     Serial.print("The distance in centimeters is ");
     Serial.print(distance);
     Serial.print("\n");
     i = 0;
   }
-
+  
   if(distance < 5){
     obstacle = 1;
   }else{
@@ -226,4 +296,61 @@ int ultrasonic(){
   }
 
   return obstacle;
+}
+
+int bluetooth(){
+
+  static int drive = 0;
+
+  HM10.listen();  // listen the HM10 port
+  if (HM10.available() > 0){
+    Serial.println("reading...");
+    HM10.write("reading...");
+  }
+  
+
+  while (HM10.available() > 0){
+    //Serial.println("TEST");
+    appData = HM10.read();
+    inData = String(appData);  // save the data in string format
+    //Serial.write(appData);
+    //Serial.println(inData);
+  }
+  if( inData == "S") 
+  {
+  Serial.println("Tractor stopped");
+  HM10.write("Tractor stopped");
+  //insert tractor instructions for switching off
+  drive = 0;
+  }
+  if ( inData == "G") 
+  {
+  Serial.println("Tractor Moving..");
+  HM10.write("Tractor moving");
+  //insert tractor instructions for On
+  drive = 1;
+  }
+
+  inData = "";
+  return drive;
+}
+
+// Turn function
+void turn(float currYaw, float prevYaw, int dt){
+  // left yaw is positive, right yaw is negative
+
+  // turn left currYaw + 90
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+
+
+  // turn right currYaw - 90
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  
 }
