b01c63e2f220ba8c44918717a25d1c78d9ba0ee9
Jai Deshmukh <67594689+djai-J@users.noreply.github.com>
2022-11-14 13:28:45 -0500

    updated button_main

--- GUI/button_main.py
+++ GUI/button_main.py
@@ -9,13 +9,26 @@ address = "34:08:E1:1A:03:62"
 MODEL_NBR_UUID = "0522506A-7F0E-90AF-18DA-3037D2FEBFAE"
 
 client = BleakClient(address) 
+start_msg = bytes('G', 'utf-8')
+stop_msg = bytes('S', 'utf-8')
+check_statement = False
+
 async def main(address):
-	
 	try:
 		await client.connect()
+		#await client.start_notify(MODEL_NBR_UUID, btn_a_handler)
 		print('Connected again!')
 	except Exception as e:
 		print(e)
+
+
+
+# async def statement(address, mssg):
+# 	if(mssg == 'G'):
+# 		await client.write_gatt_char(MODEL_NBR_UUID, start_msg)
+# 	else if(mssg == 'S'):
+# 		await client.write_gatt_char(MODEL_NBR_UUID, stop_msg)
+
 
 pygame.font.init()
 
@@ -45,26 +58,61 @@ start_button = button.Button(50, 20, start_img, 0.5)
 stop_button = button.Button(250, 20, stop_img, 0.5)
 exit_button = button.Button(450, 20, exit_img, 0.5)
 
+
+async def START(address):
+	try:
+		await client.write_gatt_char(MODEL_NBR_UUID, start_msg)
+	except Exception as e:
+		print(e)
+
+async def STOP(address):
+	try:
+		await client.write_gatt_char(MODEL_NBR_UUID, stop_msg)
+	except Exception as e:
+		print(e)
+
+async def DISCONNECT(address):
+	try:
+		await client.disconnect()
+	except Exception as e:
+		print(e)
+
+# def btn_b_handler(sender, data):
+# 	"""Simple notification handler for btn b events."""
+# 	print("{0}: {1}".format(sender, data))
+# 	if int.from_bytes(data, byteorder='little', signed=False) > 0:
+# 		loop.create_task(client.disconnect())
+
 #game loop
 run = True
-asyncio.run(main(address)) 
+asyncio.run(main(address))
 while run:
 
-
+	#loop = asyncio.get_event_loop()
+	#loop.run_until_complete(run(address, True)) 
 	#asyncio.run(main(address))  
 
 
+
+	asyncio.run(main(address))
 
 	screen.fill((202, 228, 241))
 	screen.blit(text1, textRect1) 
 
 	if start_button.draw(screen):
 		print('START')
+		#await client.write_gatt_char(MODEL_NBR_UUID, start_msg)
+		asyncio.run(STOP(address))
+		#check_statement = False
 	if stop_button.draw(screen):
 		print('STOP')
+		#await client.write_gatt_char(MODEL_NBR_UUID, stop_msg) 
+		asyncio.run(START(address))
+		#check_statement = True
 	if exit_button.draw(screen):
 		print('EXIT')
-		client.disconnect()
+		asyncio.run(DISCONNECT(address))
+		#await client.disconnect()
 		print('Disconnected')
 		run = False
 
